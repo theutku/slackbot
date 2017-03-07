6ceb79da8090ec7b7749700caf366d4114f90ded
Utku Utku <utku2107@Utkus-MacBook-Air.local>
2017-03-07 22:22:26 +0300

    heroku

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var bodyParser = require('body-parser');
 var http = require('http');
 
 var app = express();
-const port = 5000;
+
+app.set('port', (process.env.PORT || 1337));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,13 +14,12 @@ app.get('/status', (req, res, next) => {
 
 var server = http.createServer();
 
-server.listen(process.env.port || 5000);
-
-console.log('Server started listening at port: ', port);
-
-app.get('/', (req, res, next) => {
-    res.status(200).send('Slackbot App');
-})
+app.get('/', function (request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function () {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 app.post('/hello', (req, res, next) => {
     var userName = req.body.user_name;
