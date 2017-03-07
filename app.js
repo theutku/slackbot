32863454bb70bae86285b08e65063c4a0d5cbd2b
Utku TURKOGLU <uturkoglu@cinerpark.local>
2017-03-07 17:30:41 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+const port = process.env.port || 1337;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/status', (req, res, next) => {
+    res.status(200).send('App Works!');
+});
+
+app.post('/hello', (req, res, next) => {
+    var userName = req.body.user_name;
+    var botPayLoad = {
+        text: 'Hello, ' + userName + '! I saw that you called for me! This is an experimental test bot for AI Brite!'
+    }
+
+    if (userName !== 'slackbot') {
+        return res.status(200).json(botPayLoad);
+    } else {
+        return res.status(200).end();
+    }
+})
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('App started listening at port: ' + this.address().port);
+    }
+});
+
